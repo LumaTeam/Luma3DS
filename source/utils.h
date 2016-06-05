f78dd5365c93f51f3ad0d5ef51b052511907bebf
TuxSH <tuxsh@sfr.fr>
2016-06-05 20:43:49 +0200

    External .code section loading for titles and some refactoring

--- source/utils.h
+++ source/utils.h
@@ -9,7 +9,9 @@
 u32 waitInput(void);
 void mcuReboot(void);
 
-#define TICKS_PER_SEC   67027964ULL
+#define TICKS_PER_SEC       67027964ULL
+#define REG_TIMER_CNT(i)    *(vu16 *)(0x10003002 + 4 * i)
+#define REG_TIMER_VAL(i)    *(vu16 *)(0x10003000 + 4 * i)
 
 void chrono(u32 seconds);
 void stopChrono(void);
