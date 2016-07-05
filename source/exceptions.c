d5190cd788b4ca2581be757b7febae9105b87eba
TuxSH <tuxsh@sfr.fr>
2016-07-05 16:24:00 +0200

    Minor stuff (2)

--- source/exceptions.c
+++ source/exceptions.c
@@ -1,6 +1,23 @@
 /*
-*   exceptions.c
-*       by TuxSH
+*   This file is part of Luma3DS
+*   Copyright (C) 2016 Aurora Wright, TuxSH
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Additional Terms 7.b of GPLv3 applies to this file: Requiring preservation of specified
+*   reasonable legal notices or author attributions in that material or in the Appropriate Legal
+*   Notices displayed by works containing it.
 */
 
 #include "exceptions.h"
@@ -132,7 +149,7 @@ void detectAndProcessExceptionDumps(void)
             findDumpFile(path11, fileName);
             path11[17] = '/';
             memcpy(&path11[18], fileName, sizeof(fileName));
-            fileWrite((void *)dump, path11, dump[5]);
+            fileWrite((void *)dump, path11, size);
         }
 
         char arm11Str[] = "Processor:       ARM11 (core X)";
