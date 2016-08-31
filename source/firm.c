c0f042bc2a9f06d39c85971e03a7c0ea8a04812f
Aurora <davidone92@katamail.com>
2016-08-31 19:49:40 +0200

    Can not support safe_firm firmlaunches without 3ds_injector at the moment

--- source/firm.c
+++ source/firm.c
@@ -246,6 +246,8 @@ static inline u32 loadFirm(FirmwareType *firmType, FirmwareSource firmSource)
             if(firmSource != FIRMWARE_SYSNAND) 
                 error("An old unsupported EmuNAND has been detected.\nLuma3DS is unable to boot it");
 
+            if(BOOTCONFIG(5, 1)) error("SAFE_MODE is not supported on 1.x/2.x FIRM");
+
             *firmType = NATIVE_FIRM1X2X;
         }
 
