5e8990f5714c0fefdb4ec414cad0458764678ab2
Aurora <davidone92@katamail.com>
2016-09-22 15:35:28 +0200

    Fix derp

--- source/patches.c
+++ source/patches.c
@@ -102,14 +102,14 @@ void patchFirmlaunches(u8 *pos, u32 size, u32 process9MemAddr)
 
         u32 pathSize = getFileSize(pathPath);
 
-        if(pathSize > 5 && pathSize < 39)
+        if(pathSize > 5 && pathSize < 40)
         {
             u8 path[pathSize];
             fileRead(path, pathPath, 0);
             if(path[pathSize - 1] == 0xA) pathSize--;
             if(path[pathSize - 1] == 0xD) pathSize--;
 
-            if(pathSize > 5 && path[0] == '/' && memcmp(&path[pathSize - 4], ".bin", 4) == 0)
+            if(pathSize > 5 && pathSize < 38 && path[0] == '/' && memcmp(&path[pathSize - 4], ".bin", 4) == 0)
             {
                 u16 finalPath[pathSize + 1];
                 for(u32 i = 0; i < pathSize; i++)
@@ -117,7 +117,7 @@ void patchFirmlaunches(u8 *pos, u32 size, u32 process9MemAddr)
                 finalPath[pathSize] = 0;
 
                 u8 *pos_path = memsearch(off, u"sd", reboot_size, 4) + 0xA;
-                memcpy(pos_path, finalPath, pathSize);
+                memcpy(pos_path, finalPath, (pathSize + 1) * 2);
             }
         }
     }
