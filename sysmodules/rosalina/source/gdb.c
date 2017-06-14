24de7c527287ad6bd1b76b9962ef0f4330382acb
TuxSH <tuxsh@sfr.fr>
2017-06-14 10:04:09 +0200

    Fix indentation of gdb.c

--- sysmodules/rosalina/source/gdb.c
+++ sysmodules/rosalina/source/gdb.c
@@ -29,32 +29,32 @@
 
 void GDB_InitializeContext(GDBContext *ctx)
 {
-	RecursiveLock_Init(&ctx->lock);
+    RecursiveLock_Init(&ctx->lock);
 
-	RecursiveLock_Lock(&ctx->lock);
+    RecursiveLock_Lock(&ctx->lock);
 
-	svcCreateEvent(&ctx->continuedEvent, RESET_ONESHOT);
-	svcCreateEvent(&ctx->clientAcceptedEvent, RESET_STICKY);
+    svcCreateEvent(&ctx->continuedEvent, RESET_ONESHOT);
+    svcCreateEvent(&ctx->clientAcceptedEvent, RESET_STICKY);
 
-	ctx->eventToWaitFor = ctx->clientAcceptedEvent;
-	ctx->continueFlags = (DebugFlags)(DBG_SIGNAL_FAULT_EXCEPTION_EVENTS | DBG_INHIBIT_USER_CPU_EXCEPTION_HANDLERS);
+    ctx->eventToWaitFor = ctx->clientAcceptedEvent;
+    ctx->continueFlags = (DebugFlags)(DBG_SIGNAL_FAULT_EXCEPTION_EVENTS | DBG_INHIBIT_USER_CPU_EXCEPTION_HANDLERS);
 
-	RecursiveLock_Unlock(&ctx->lock);
+    RecursiveLock_Unlock(&ctx->lock);
 }
 
 void GDB_FinalizeContext(GDBContext *ctx)
 {
-	RecursiveLock_Lock(&ctx->lock);
+    RecursiveLock_Lock(&ctx->lock);
 
-	svcClearEvent(ctx->clientAcceptedEvent);
+    svcClearEvent(ctx->clientAcceptedEvent);
 
-	svcCloseHandle(ctx->clientAcceptedEvent);
-	svcCloseHandle(ctx->continuedEvent);
+    svcCloseHandle(ctx->clientAcceptedEvent);
+    svcCloseHandle(ctx->continuedEvent);
 
-	RecursiveLock_Unlock(&ctx->lock);
+    RecursiveLock_Unlock(&ctx->lock);
 }
 
 GDB_DECLARE_HANDLER(Unsupported)
 {
-	return GDB_ReplyEmpty(ctx);
+    return GDB_ReplyEmpty(ctx);
 }
