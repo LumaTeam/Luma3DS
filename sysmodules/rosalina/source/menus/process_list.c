900b9e003b89855c95a266e69f5452d91a4755b7
LiquidFenrir <liquidfenrir@outlook.fr>
2017-09-26 16:24:51 +0200

    basic ram viewer menu the actual ram viewing is not working yet

--- sysmodules/rosalina/source/menus/process_list.c
+++ sysmodules/rosalina/source/menus/process_list.c
@@ -27,6 +27,7 @@
 #include <3ds.h>
 #include "menus/process_list.h"
 #include "memory.h"
+#include "csvc.h"
 #include "draw.h"
 #include "menu.h"
 #include "utils.h"
@@ -78,10 +79,141 @@ static inline int ProcessListMenu_FormatInfoLine(char *out, const ProcessInfo *i
     return sprintf(out, "%s%-4u    %-8.8s    %s", checkbox, info->pid, info->name, commentBuf); // Theoritically PIDs are 32-bit ints, but we'll only justify 4 digits
 }
 
+static void ProcessListMenu_MemoryViewer(const ProcessInfo *info)
+{
+    s64 startAddress, textTotalRoundedSize, rodataTotalRoundedSize, dataTotalRoundedSize;
+    u32 totalSize;
+    const u32 destAddress = 0x00100000;
+    Handle processHandle;
+
+    Result res = OpenProcessByName(info->name, &processHandle);
+
+    if (R_SUCCEEDED(res))
+    {
+        svcGetProcessInfo(&textTotalRoundedSize, processHandle, 0x10002);
+        svcGetProcessInfo(&rodataTotalRoundedSize, processHandle, 0x10003);
+        svcGetProcessInfo(&dataTotalRoundedSize, processHandle, 0x10004);
+
+        totalSize = (u32)(textTotalRoundedSize + rodataTotalRoundedSize + dataTotalRoundedSize);
+
+        svcGetProcessInfo(&startAddress, processHandle, 0x10005);
+
+        res = svcMapProcessMemoryEx(processHandle, destAddress, (u32)startAddress, totalSize);
+
+        if (R_SUCCEEDED(res))
+        {
+            #define ROWS_PER_SCREEN 0x10
+            #define BYTES_PER_ROW 0x10
+
+            u8 arr[BYTES_PER_ROW] = {0};
+
+            const u32 rows = totalSize/BYTES_PER_ROW;
+
+            u32 scroll = 0, selected = 0;
+            #define SELECTED_ROW ((selected - (selected%BYTES_PER_ROW))/BYTES_PER_ROW)
+
+            Draw_Lock();
+            Draw_ClearFramebuffer();
+            Draw_FlushFramebuffer();
+            Draw_Unlock();
+
+            do
+            {
+                Draw_Lock();
+                Draw_DrawString(10, 10, COLOR_TITLE, "Memory viewer");
+                Draw_DrawFormattedString(10, 26, COLOR_WHITE, "Use D-PAD to navigate.");
+
+                for (u32 i = 0; i < rows; i++)
+                {
+                    if (rows <= ROWS_PER_SCREEN)
+                        break;
+
+                    if (scroll > SELECTED_ROW)
+                        scroll--;
+
+                    if ((i <= SELECTED_ROW) && \
+                       ((SELECTED_ROW - scroll) >= ROWS_PER_SCREEN) && \
+                       (scroll != (rows - ROWS_PER_SCREEN)))
+                        scroll++;
+                }
+
+                for (u32 i = scroll; i < (scroll + ROWS_PER_SCREEN); i++)
+                {
+                    // u8 ** membuf = (u8**)startAddress;
+                    u32 offset = i-scroll;
+                    u32 y = 44 + offset*12;
+
+                    if (i > totalSize)
+                        Draw_DrawFormattedString(10, y, COLOR_WHITE, "                                                ");
+                    else
+                    {
+                        Draw_DrawFormattedString(10, y, COLOR_WHITE, "%.8lx | ", i*BYTES_PER_ROW);
+                        for (int cursor = 0; cursor < BYTES_PER_ROW; cursor++)
+                        {
+                            Draw_DrawFormattedString(10+66+cursor*14+(cursor >= BYTES_PER_ROW/2)*10, y, i*BYTES_PER_ROW +cursor == selected ? COLOR_GREEN : COLOR_WHITE, "%.2x",
+                            // membuf[i*BYTES_PER_ROW][cursor]);
+                            arr[cursor]);
+                        }
+                    }
+                }
+
+                Draw_FlushFramebuffer();
+                Draw_Unlock();
+
+                u32 pressed = waitInputWithTimeout(1000);
+
+                if(pressed & BUTTON_LEFT) // -1 byte
+                {
+                    if (selected != 0)
+                        selected--;
+                }
+                else if(pressed & BUTTON_RIGHT) // +1 byte
+                {
+                    if (selected != totalSize)
+                        selected++;
+                }
+                else if(pressed & BUTTON_UP) // -1 line (0x10 bytes)
+                {
+                    if (selected >= BYTES_PER_ROW)
+                        selected -= BYTES_PER_ROW;
+                }
+                else if(pressed & BUTTON_DOWN) // +1 line (0x10 bytes)
+                {
+                    if (selected <= totalSize-BYTES_PER_ROW)
+                        selected += BYTES_PER_ROW;
+                }
+                if(pressed & BUTTON_L1) // -1 screen (0x100 bytes)
+                {
+                    if (selected >= (BYTES_PER_ROW*ROWS_PER_SCREEN))
+                        selected -= (BYTES_PER_ROW*ROWS_PER_SCREEN);
+                }
+                if(pressed & BUTTON_R1) // +1 screen (0x100 bytes)
+                {
+                    if (selected <= totalSize-(BYTES_PER_ROW*ROWS_PER_SCREEN))
+                        selected += (BYTES_PER_ROW*ROWS_PER_SCREEN);
+                }
+                else if(pressed & BUTTON_B) // go back to the list
+                    break;
+
+                if (selected == totalSize)
+                    selected -= 1;
+            }
+            while(!terminationRequest);
+
+            Draw_Lock();
+            Draw_ClearFramebuffer();
+            Draw_FlushFramebuffer();
+            Draw_Unlock();
+
+            svcUnmapProcessMemoryEx(processHandle, destAddress, totalSize);
+        }
+    }
+}
+
 static inline void ProcessListMenu_HandleSelected(const ProcessInfo *info)
 {
     if(!gdbServer.super.running || info->isZombie)
-        return;
+        ProcessListMenu_MemoryViewer(info);
 
     u32 id;
     for(id = 0; id < MAX_DEBUG && (!(gdbServer.ctxs[id].flags & GDB_FLAG_SELECTED) || gdbServer.ctxs[id].pid != info->pid); id++);
