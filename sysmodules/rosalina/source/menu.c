6d14b4167b9f209bee9b79bd3459140050432462
Pirater12 <pokemonemerald0@gmail.com>
2017-12-19 15:28:01 +0530

    Oops

--- sysmodules/rosalina/source/menu.c
+++ sysmodules/rosalina/source/menu.c
@@ -170,6 +170,7 @@ void menuEnter(void)
         svcSleepThread(5 * 1000 * 100LL);
         Draw_SetupFramebuffer();
         Draw_ClearFramebuffer();
+		srvPublishToSubscriber(0x400, 0);
     }
 }
 
@@ -184,6 +185,7 @@ void menuLeave(void)
         Draw_RestoreFramebuffer();
         Draw_Unlock();
         svcKernelSetState(0x10000, 1);
+		srvPublishToSubscriber(0x401, 0);
     }
 }
 
