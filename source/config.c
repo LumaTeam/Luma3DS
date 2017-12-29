bfec874a7c7e95e90f046eafca31b3e1d8698ee2
Mikaela RJ Szekely <qyriad@gmail.com>
2017-12-28 22:02:06 -0500

    Add colon in option that I forgot

--- source/config.c
+++ source/config.c
@@ -81,7 +81,7 @@ void configMenu(bool oldPinStatus, u32 oldPinMode)
     static const char *multiOptionsText[]  = { "Default EmuNAND: 1( ) 2( ) 3( ) 4( )",
                                                "Screen brightness: 4( ) 3( ) 2( ) 1( )",
                                                "Splash: Off( ) Before( ) After( ) payloads",
-                                               "Splash duration 1( ) 3( ) 5( ) 7( ) seconds",
+                                               "Splash duration: 1( ) 3( ) 5( ) 7( ) seconds",
                                                "PIN lock: Off( ) 4( ) 6( ) 8( ) digits",
                                                "New 3DS CPU: Off( ) Clock( ) L2( ) Clock+L2( )",
                                              };
@@ -204,7 +204,7 @@ void configMenu(bool oldPinStatus, u32 oldPinMode)
         { .posXs = {19, 24, 29, 34}, .visible = isSdMode },
         { .posXs = {21, 26, 31, 36}, .visible = true },
         { .posXs = {12, 22, 31, 0}, .visible = true  },
-        { .posXs = {18, 23, 28, 33}, .visible = true },
+        { .posXs = {19, 24, 29, 34}, .visible = true },
         { .posXs = {14, 19, 24, 29}, .visible = true },
         { .posXs = {17, 26, 32, 44}, .visible = ISN3DS },
     };
