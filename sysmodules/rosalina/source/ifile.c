eab9a31dff03159c954be3af5cf2d057262af6f7
piepie62 <christopherfeger@yahoo.com>
2019-04-15 13:08:53 -0700

    Fix reading past the end of a file

--- sysmodules/rosalina/source/ifile.c
+++ sysmodules/rosalina/source/ifile.c
@@ -71,7 +71,7 @@ Result IFile_Read(IFile *file, u64 *total, void *buffer, u32 len)
   while (1)
   {
     res = FSFILE_Read(file->handle, &read, file->pos, buf, left);
-    if (R_FAILED(res))
+    if (R_FAILED(res) || read == 0)
     {
       break;
     }
