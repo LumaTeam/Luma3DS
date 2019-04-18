687dfddd9f0c5fb898652292a1b8e6098e5e1ad9
TuxSH <tuxsh@sfr.fr>
2019-04-19 00:37:20 +0200

    HBLDR_DebugNextApplicationByForce changes

--- sysmodules/loader/source/hbldr.h
+++ sysmodules/loader/source/hbldr.h
@@ -11,3 +11,4 @@ Result HBLDR_LoadProcess(Handle *outCodeSet, u32 textAddr, u32 kernelFlags, u64 
 Result HBLDR_SetTarget(const char* path);
 Result HBLDR_SetArgv(const void* buffer, size_t size);
 Result HBLDR_PatchExHeaderInfo(ExHeader_Info *exheaderInfo);
+Result HBLDR_DebugNextApplicationByForce(bool dryRun);
