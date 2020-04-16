8785fd0236732d7fc20f5595beb881dc47fd265d
TuxSH <tuxsh@sfr.fr>
2020-04-16 21:05:33 +0100

    Use -Os for rosalina, pm, sm. Decrease gdb buffer sizes, etc.

--- sysmodules/rosalina/include/gdb.h
+++ sysmodules/rosalina/include/gdb.h
@@ -18,14 +18,14 @@
 
 #define MAX_DEBUG           3
 #define MAX_DEBUG_THREAD    127
-#define MAX_BREAKPOINT      256
+#define MAX_BREAKPOINT      64
 
 #define MAX_TIO_OPEN_FILE   32
 
 // 512+24 is the ideal size as IDA will try to read exactly 0x100 bytes at a time. Add 4 to this, for $#<checksum>, see below.
 // IDA seems to want additional bytes as well.
 // 1024 is fine enough to put all regs in the 'T' stop reply packets
-#define GDB_BUF_LEN 2048
+#define GDB_BUF_LEN 1024
 
 #define GDB_HANDLER(name)           GDB_Handle##name
 #define GDB_QUERY_HANDLER(name)     GDB_HANDLER(Query##name)
