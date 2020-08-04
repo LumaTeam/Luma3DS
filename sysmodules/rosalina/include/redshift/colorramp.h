5884679a97b2c903ad1538b7bd4705e489060543
DullPointer <43319229+DullPointer@users.noreply.github.com>
2020-08-04 19:04:35 +0100

    Remove unused redshift bloat

--- sysmodules/rosalina/include/redshift/colorramp.h
+++ sysmodules/rosalina/include/redshift/colorramp.h
@@ -26,7 +26,5 @@
 
 void colorramp_fill(uint16_t *gamma_r, uint16_t *gamma_g, uint16_t *gamma_b,
 		    int size, const color_setting_t *setting);
-void colorramp_fill_float(float *gamma_r, float *gamma_g, float *gamma_b,
-			  int size, const color_setting_t *setting);
 
 #endif /* ! REDSHIFT_COLORRAMP_H */
--- sysmodules/rosalina/source/redshift/colorramp.c
+++ sysmodules/rosalina/source/redshift/colorramp.c
@@ -308,22 +308,4 @@ colorramp_fill(uint16_t *gamma_r, uint16_t *gamma_g, uint16_t *gamma_b,
 	}
 }
 
-void
-colorramp_fill_float(float *gamma_r, float *gamma_g, float *gamma_b,
-		     int size, const color_setting_t *setting)
-{
-	/* Approximate white point */
-	float white_point[3];
-	float alpha = (setting->temperature % 100) / 100.0;
-	int temp_index = ((setting->temperature - 1000) / 100)*3;
-	interpolate_color(alpha, &blackbody_color[temp_index],
-			  &blackbody_color[temp_index+3], white_point);
-
-	for (int i = 0; i < size; i++) {
-		gamma_r[i] = F((double)gamma_r[i], 0);
-		gamma_g[i] = F((double)gamma_g[i], 1);
-		gamma_b[i] = F((double)gamma_b[i], 2);
-	}
-}
-
 #undef F
