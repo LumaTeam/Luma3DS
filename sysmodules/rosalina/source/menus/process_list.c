d83008a79b448fcdc9b651fb521b7419c6705afe
George <33588728+George-lewis@users.noreply.github.com>
2021-12-30 15:00:47 -0500

    Add new info text

--- sysmodules/rosalina/source/menus/process_list.c
+++ sysmodules/rosalina/source/menus/process_list.c
@@ -419,15 +419,19 @@ static void ProcessListMenu_MemoryViewer(const ProcessInfo *info)
                 // ------------------------------------------
 
                 // Location
+                const u32 infoY = instructionsY + SPACING_Y;
+                viewerY += SPACING_Y;
                 if(codeAvailable && heapAvailable)
-                {
-                    const u32 infoY = instructionsY + SPACING_Y;
-                    viewerY += SPACING_Y;
+                {    
                     Draw_DrawString(10, infoY, COLOR_WHITE, "Press L or R to switch between heap and code.");
                     if((u32)menus[MENU_MODE_NORMAL].buf == heapDestAddress)
                         Draw_DrawString(10 + SPACING_X * 31, infoY, COLOR_GREEN, "heap");
                     if((u32)menus[MENU_MODE_NORMAL].buf == codeDestAddress)
                         Draw_DrawString(10 + SPACING_X * 40, infoY, COLOR_GREEN, "code");
+                } else {
+                    Draw_DrawString(10, infoY, COLOR_WHITE, "SELECT to dump memory, START to toggle ASCII view.");
+                    if (ascii)
+                        Draw_DrawString(10 + SPACING_X * 39, infoY, COLOR_GREEN, "ASCII");
                 }
                 // ------------------------------------------
 
