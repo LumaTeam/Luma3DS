5559b1a421beb2f8d44be2f009cdea0cb7294f15
Core 2 Extreme <45873899+Core-2-Extreme@users.noreply.github.com>
2022-11-16 19:36:52 +0900

    Applied suggestions
    Co-authored-by: PabloMK7 <hackyglitch2@gmail.com>

--- arm9/source/patches.c
+++ arm9/source/patches.c
@@ -728,7 +728,7 @@ u32 patchCore2(u8 *pos, u32 size)
     So we can always create thread on core #2 even process is not running on BASE memory region.
     */
     //cmp r9, #0x2 -> cmp r9, #0x4
-    offset[1] = __builtin_bswap32(0x040059E3);
+    offset[1] = 0xE3590004
 
     return 0;
 }
