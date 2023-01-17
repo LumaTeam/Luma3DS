6592b516a493e0b6a97ce96d18acd4c8889645b4
TuxSH <1922548+TuxSH@users.noreply.github.com>
2023-01-17 23:48:07 +0000

    rosalina: refactor screen filter code
    https://i.kym-cdn.com/photos/images/original/001/264/842/220.png

--- sysmodules/rosalina/include/redshift/colorramp.h
+++ sysmodules/rosalina/include/redshift/colorramp.h
@@ -22,11 +22,8 @@
 
 #include <stdint.h>
 
-#include "redshift.h"
+//#include "redshift.h"
 
-void colorramp_fill(uint16_t *gamma_r, uint16_t *gamma_g, uint16_t *gamma_b,
-		    int size, const color_setting_t *setting);
-void colorramp_fill_float(float *gamma_r, float *gamma_g, float *gamma_b,
-			  int size, const color_setting_t *setting);
+void colorramp_get_white_point(float *out_white_point, int temperature); // not in original code
 
 #endif /* ! REDSHIFT_COLORRAMP_H */
