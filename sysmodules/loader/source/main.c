035c50c6256aafde8ef78b95f190a1ab20240dec
TuxSH <1922548+TuxSH@users.noreply.github.com>
2023-08-12 18:00:08 +0200

    Fix support for all older versions
    - Move emuNAND hook to ITCM, fixing previously untested emuNAND support
      for 5.0 (and possibly more versions). This was a very long-standing
      bug
    - Fix booting on versions 4.x to 8.x, and restore full support for 5.x
      to 8.x. All Arm11 custom sysmodules and k11ext have been disabled on
      4.x because the Luma3DS v13.0 changes couldn't be ported (this means
      no Rosalina and no region-free and such, and while I could restore
      some of the functionality, you should just update *after* installing
      Luma, like you've been instructed to)

--- sysmodules/loader/source/main.c
+++ sysmodules/loader/source/main.c
@@ -59,7 +59,11 @@ static inline void loadCFWInfo(void)
         if (numKips >= 6)
             panic(0xDEADCAFE);
 
-        config = 0; // all options 0
+#ifndef BUILD_FOR_EXPLOIT_DEV
+        config = 1u << PATCHVERSTRING; // all options 0, except maybe the MSET version display patch
+#else
+        config = 0;
+#endif
         multiConfig = 0;
         bootConfig = 0;
         isN3DS = OS_KernelConfig->app_memtype >= 6;
