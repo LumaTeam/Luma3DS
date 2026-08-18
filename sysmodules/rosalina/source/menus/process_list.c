1b9c0eb8ce8e3dcb9e08f980ef4a788171f9152d
god88 <6161433+god88@users.noreply.github.com>
2026-08-18 02:17:46 +0200

    rosalina: keep memory search position on miss

--- sysmodules/rosalina/source/menus/process_list.c
+++ sysmodules/rosalina/source/menus/process_list.c
@@ -388,7 +388,11 @@ static void ProcessListMenu_MemoryViewer(const ProcessInfo *info)
                 u8 * startpos = (u8*)((u32)menus[MENU_MODE_NORMAL].buf + menus[MENU_MODE_NORMAL].selected);
                 u32 size = menus[MENU_MODE_NORMAL].max - menus[MENU_MODE_NORMAL].selected;
                 if (size >= searchPatternSize)
-                    menus[MENU_MODE_NORMAL].selected = (u32)memsearch(startpos, searchPattern, size, searchPatternSize) - (u32)menus[MENU_MODE_NORMAL].buf;
+                {
+                    u8 *match = memsearch(startpos, searchPattern, size, searchPatternSize);
+                    if (match != NULL)
+                        menus[MENU_MODE_NORMAL].selected = (u32)match - (u32)menus[MENU_MODE_NORMAL].buf;
+                }
             }
 
             menus[MENU_MODE_SEARCH].buf = searchPattern;
